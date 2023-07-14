247b810a9cd4556a3b5f94a0cb63ed1e79a370e2
madara1319 <mateusz.bik@op.pl>
2023-07-14 20:58:20 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("Siema");
+
+//typ danych number
+let myVariable = 10;
+console.log("Moja liczba " + myVariable + " Jest typu " + typeof myVariable);
+
+let myName = "Benedykt";
+console.log("Moj string " + myName + " Jest typu " + typeof myName);
+
+myVariable = 11;
+myName = "Mieszko";
+sum = myVariable + 5;
+greeting = myName + " ,witaj w świecie JS!";
+console.log(sum);
+console.log(greeting);
+const block = document.querySelector(".box--js");
+block.innerHTML = `chuj`;
